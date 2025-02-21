d9e103d9ffe8371ccfc44e59bf6f9799d0e6411b
Katu <122546760+shalo-sakibara@users.noreply.github.com>
2025-02-21 18:04:24 +0300

    update_DB
    Протестировал все функции. Пока что убрал проверку на sql-инъекцию(надо будет доработать). Довел до ума несколько функций.

--- DB.py
+++ DB.py
@@ -9,24 +9,19 @@ class Data_Base:
                 3: "Images", 
                 4: "Charisma",
                 5: "Atmosphere"}
-    def _check_parametrs(command):
+    
+    
+    
+    def _check_parametrs(self, command):
         """Проверка команды на SQL-инъекцию"""
-        # Паттерны, указывающие на возможные инъекции
-        sql_patterns = [
-            r"(\b(SELECT|INSERT|UPDATE|DELETE|WHERE|OR|AND|DROP|JOIN|EXEC|UNION|;|'|--|#|\\)\b)",
-            r"(--|\bOR\b|\bAND\b|;|')"
-        ]
-        
-        for pattern in sql_patterns:
-            if re.search(pattern, command, re.IGNORECASE):
-                return False
         return True
         
         
     
     def __command(self, command, param=None):
         """Функция проверяет команду на sql-инъекцию, и исполняет её, закрывая подключение к БД, если всё нормально, иначе, записывает в логи, что была введена не корректная команда"""
-        if self._check_parametrs(command):
+        # if self._check_parametrs(command):
+        if self._check_parametrs(command=command):
             try:
                 conn = self.__create_connection(self.data_base)
                 cursor = conn.cursor()
@@ -48,8 +43,8 @@ class Data_Base:
                 print(e)
                 return False
         else:
-            return f"<!> Не корректная сторока: {command}"
-
+            print (f"<!> Не корректная сторока: {command}")
+            return False
     
     def __create_connection(self, db_file): 
         return sqlite3.connect(db_file)
@@ -103,9 +98,11 @@ class Data_Base:
     def get_estimations(self, song_name): # <?> Возможно нужно будет удалить
         """Функция, которая возвращает всю нужную информацию об оценках, по названию трека"""
         """Возращается список. Первое число-сумма, второе число-кол-во оценок"""
-        command = '''SELECT Rhythm, Rhymes, Wealth, Images, Charisma, Atmosphere FROM ratings 
-                 INNER JOIN tracks ON ratings.track_id = tracks.id 
-                 WHERE tracks.name = ?'''
+        command = '''SELECT Rhythm, Rhymes, Wealth, Images, Charisma, Atmosphere 
+             FROM ratings 
+             INNER JOIN tracks ON ratings.track_id = tracks.id 
+             INNER JOIN artists ON tracks.artist_id = artists.id 
+             WHERE tracks.name = ?'''
         arr = self.__command(command=command, param=(song_name,))
         arr = arr[0]
         arr = [list(map(int, cat.split('.'))) for cat in arr]
@@ -115,22 +112,38 @@ class Data_Base:
     def get_autor(self, name_autor= None, artist_id=None):
         """Функция, которая возвращает всю нужную информацию про автора"""
         if artist_id == None and name_autor != None:
-            command =  '''SELECT name, photo FROM artists 
+            command =  '''SELECT id, name, photo FROM artists 
                     WHERE name = ?'''
-            return self.__command(command=command, param=(name_autor,))
+            result = self.__command(command=command, param=(name_autor,))
+            if len(result) != 0:
+                return result[0]
+            else:
+                return ()
         elif artist_id != None and name_autor == None:
-            command =  '''SELECT name, photo FROM artists 
+            command =  '''SELECT id, name, photo FROM artists 
                     WHERE id = ?'''
-            return self.__command(command=command, param=(artist_id,))
+            result = self.__command(command=command, param=(artist_id,))
+            if len(result) != 0:
+                return result[0]
+            else:
+                return ()
         
         
     def get_song(self, song_name):
         """Функция, которая возвращает всю нужную информацию про трек"""
-        command = """SELECT artist_id, name, release_date, photo FROM tracks WHERE name = ?"""
+        command = """SELECT artist_id, id, name, release_date, photo FROM tracks WHERE name = ?"""
         result = self.__command(command=command, param=(song_name,))
-        artist_name = self.get_autor(artist_id=result[0][0])
-        return result[0][1:] + (artist_name,)
+        if result != False:
+            artist_name = self.get_autor(artist_id=result[0][1])[1]
+            return result[0][1:] + (artist_name,)
         
+        
+    def _set_ratings(self, track_id):
+        """Инициализация пустых оценок для трека"""
+        command = """INSERT INTO ratings (track_id, Rhythm, Rhymes, Wealth, Images, Charisma, Atmosphere) 
+                    VALUES (?, '0.0', '0.0', '0.0', '0.0', '0.0', '0.0')"""
+        self.__command(command=command, param=(track_id,))
+    
     
     def set_estimations_to_song(self, name_song:str, estimations:list):
         """Добавление оценок пользователя на трек"""
@@ -140,29 +153,32 @@ class Data_Base:
             old_estimations[i][1] += 1
         a = tuple('.'.join(map(str, i)) for i in old_estimations)
         command = """UPDATE ratings 
-                SET Rhythm = ?, 
-                    Rhymes = ?, 
-                    Wealth = ?, 
-                    Images = ?, 
-                    Charisma = ?, 
-                    Atmosphere = ?, 
-                    ocenka = ?
-                INNER JOIN tracks ON ratings.track_id = tracks.id 
-                WHERE tracks.name = ?"""
+             SET Rhythm = ?, 
+                 Rhymes = ?, 
+                 Wealth = ?, 
+                 Images = ?, 
+                 Charisma = ?, 
+                 Atmosphere = ? 
+             WHERE track_id = (SELECT id FROM tracks WHERE name = ?)"""
         self.__command(command=command, param=a + (name_song,))
         
 
     def _set_autor(self, name, path_to_photo):
         """Добавление нового автора"""
-        command = """INSERT INTO artists(name, photo) VALUES(?, ?)"""
-        self.__command(command=command, param=(name, path_to_photo))
+        if len(self.get_autor(name_autor=name)) == 0:
+            command = """INSERT INTO artists(name, photo) VALUES(?, ?)"""
+            self.__command(command=command, param=(name, path_to_photo))
+        
     
     
     def _set_Song(self, artist_name, name, release_date):
         """Добавление нового трека"""
-        artist_id = self.get_autor(artist_name)[0][1]
+        self._set_autor(name=artist_name, path_to_photo='deff_photo')
+        artist_id = self.get_autor(name_autor=artist_name)[0]
         command = """INSERT INTO tracks (artist_id, name, release_date) VALUES(?, ?, ?)"""
         self.__command(command=command, param=(artist_id, name, release_date))
+        self._set_ratings(self.get_song(song_name=name)[0])
+        
         
     
     def _remove_Song(self, song_name):
@@ -170,4 +186,3 @@ class Data_Base:
         command = """DELETE FROM tracks WHERE name = ?"""
         self.__command(command=command, param=(song_name,))
         
-
